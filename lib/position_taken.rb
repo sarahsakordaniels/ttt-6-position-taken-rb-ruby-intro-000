416231210b89c287a8fe700edfe772873cbd3414
Sarah Daniels <ssdaniels13@gmail.com>
2019-01-24 01:28:23 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ def position_taken?(board, index)
   if
     board == [" ", " ", " ", " ", " ", " ", " ", " ", " "] || ["", "", "", "", "", "", "", "", ""] ||
     [nil, " ", " ", " ", " ", " ", " ", " ", " "]
-
+    index = array[0]
 
 
 end
